23375c3ac476263ae9049d37dd43f800cc3e6d12
Henil Chhipani <henilchhipani@gmail.com>
2025-01-29 12:55:13 +0530

    adding override removeEldestEntry for better cashing

--- app/src/main/java/com/henil/dogimagegen/data/LruCache.kt
+++ app/src/main/java/com/henil/dogimagegen/data/LruCache.kt
@@ -1,20 +1,31 @@
 package com.henil.dogimagegen.data
+//
+//class LruCache<K, V>(private val maxSize: Int) : LinkedHashMap<K, V>(maxSize, 0.75f, true) {
+//
+//    private val cache: LinkedHashMap<K, V> = LinkedHashMap(0, 0.75f, true)
+//
+//    fun put(key: K, value: V) {
+//        if (cache.size >= maxSize) {
+//            val leastRecentlyUsedKey = cache.keys.iterator().next()
+//            cache.remove(leastRecentlyUsedKey)
+//        }
+//        cache[key] = value
+//    }
+//    override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, V>?): Boolean {
+//        return size > maxSize
+//    }
+//    fun get(key: K): V? = cache[key]
+//
+//    fun getAll(): List<V> = cache.values.toList()
+//
+//    fun clear() = cache.clear()
+//}
 
-class LruCache<K, V>(private val maxSize: Int) {
+class LruCache<K, V>(private val maxSize: Int) : LinkedHashMap<K, V>(maxSize, 0.75f, true) {
 
-    private val cache: LinkedHashMap<K, V> = LinkedHashMap(0, 0.75f, true)
-
-    fun put(key: K, value: V) {
-        if (cache.size >= maxSize) {
-            val leastRecentlyUsedKey = cache.keys.iterator().next()
-            cache.remove(leastRecentlyUsedKey)
-        }
-        cache[key] = value
+    override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, V>?): Boolean {
+        return size > maxSize
     }
 
-    fun get(key: K): V? = cache[key]
-
-    fun getAll(): List<V> = cache.values.toList()
-
-    fun clear() = cache.clear()
+    fun getAll(): List<V> = values.toList()
 }
